516690c098d831476a92a7116d7f8d5f8815fc3a
CodeJr10 <aadityamalap666@gmail.com>
2025-06-27 16:31:51 +0530

    feat:Implement base  getThumbnails function

---
+++ Opify.js
@@ -0,0 +1,31 @@
+// find thumbnail
+// add overlay
+// add overlay to that thumbnail
+
+function getThumbnails() {
+  // Get all thumbnail images from the Main YouTube page
+  const getThumbnailImages = document.querySelectorAll(
+    "ytd-thumbnail a > yt-image > img.yt-core-image"
+  );
+
+  const allImages = [...Array.from(getThumbnailImages)];
+
+  const targetAspectRatio = [16 / 9, 4 / 3];
+
+  const allowErrorMargin = 0.02;
+
+  // to avoid overlay on profile pictures, icons etc and only focus on video thumbnails
+  const filteredThumbnails = allImages.filter((image) => {
+    // skip broken images
+    if (image.height === 0) return false;
+
+    const aspectRatio = image.width / image.height;
+
+    // calculate to see whether the aspect ratio of image is within the target range of 16:9 or 4:3
+    let isCorrectAspectRatio =
+      Math.abs(aspectRatio - targetAspectRatio[0]) < allowErrorMargin ||
+      Math.abs(aspectRatio - targetAspectRatio[1]) < allowErrorMargin;
+
+    return isCorrectAspectRatio;
+  });
+}
