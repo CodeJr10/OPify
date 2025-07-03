f8ecaff2effc7ccef9dadb47519d68ec70197988
CodeJr10 <aadityamalap666@gmail.com>
2025-07-03 13:55:19 +0530

    feat: implement function to save user settings

--- popup.js
+++ popup.js
@@ -2,11 +2,25 @@
 function loadSettings() {
   chrome.storage.local.get(
     {
-      extensionIsDisabled: false,
+      extensionIsEnabled: true, // default value
     },
     (data) => {
-      document.getElementById("disableExtension").checked =
-        !data.extensionIsDisabled;
+      document.getElementById("enableExtension").checked =
+        data.extensionIsEnabled;
     }
   );
+}
+
+function saveSettings() {
+  const data = {
+    extensionIsEnabled: document.getElementById("enableExtension").checked,
+  };
+
+  chrome.storage.local.set(data, () => {
+    if (chrome.runtime.lastError) {
+      console.error("Error saving settings", chrome.runtime.lastError);
+    } else {
+      console.log("Settings are saved");
+    }
+  });
 }
